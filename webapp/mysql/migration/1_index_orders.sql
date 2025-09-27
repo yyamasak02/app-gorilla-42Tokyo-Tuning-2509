e1ecf652d90bb930b5644e26a2773e8f6d853e42
yuhe-02 <domineter4649@gmail.com>
2025-09-27 22:35:44 +0900

    [UPDATE] 注文API

--- webapp/mysql/migration/1_index_orders.sql
+++ webapp/mysql/migration/1_index_orders.sql
@@ -5,3 +5,7 @@ CREATE INDEX idx_orders_uid_pid
 -- orders の shipped_status + product_id + order_id に複合インデックス
 CREATE INDEX idx_orders_status_product_orderid
    ON orders (shipped_status, product_id, order_id);
+CREATE INDEX idx_order_created_at_pid ON orders(order_id, created_at);
+CREATE INDEX idx_order_created_at_pid ON orders(order_id, shipped_status);
+CREATE INDEX idx_order_created_at_pid ON orders(order_id, arrived_at);
+CREATE INDEX idx_user_id_pid ON orders(user_id);
