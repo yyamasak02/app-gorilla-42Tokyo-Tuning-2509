31b4b18d402975b67d0bb51e25dbb8dd6726efc4
yuhe-02 <domineter4649@gmail.com>
2025-09-28 08:36:29 +0900

    [FIX] sql syntax error

--- webapp/mysql/migration/4_add_order_product_metrics.sql
+++ webapp/mysql/migration/4_add_order_product_metrics.sql
@@ -4,15 +4,23 @@ ALTER TABLE orders
 ADD COLUMN product_weight INT,
 ADD COLUMN product_value INT;
 
-SET @updated_rows = 1;
-WHILE @updated_rows > 0 DO
-    UPDATE orders o
-    JOIN products p ON o.product_id = p.product_id
-    SET o.product_weight = p.weight, o.product_value = p.value
-    WHERE o.order_id IN (
-        SELECT order_id FROM (
-            SELECT order_id FROM orders WHERE product_weight IS NULL LIMIT 10000
-        ) as t
-    );
-    SELECT ROW_COUNT() INTO @updated_rows;
-END WHILE;
+DELIMITER //
+CREATE PROCEDURE BatchUpdateOrdersMetrics()
+BEGIN
+    DECLARE updated_rows INT DEFAULT 1;
+    WHILE updated_rows > 0 DO
+        UPDATE orders o
+        JOIN products p ON o.product_id = p.product_id
+        SET o.product_weight = p.weight, o.product_value = p.value
+        WHERE o.order_id IN (
+            SELECT order_id FROM (
+                SELECT order_id FROM orders WHERE product_weight IS NULL LIMIT 10000
+            ) as t
+        );
+        SELECT ROW_COUNT() INTO updated_rows;
+    END WHILE;
+END //
+DELIMITER ;
+
+CALL BatchUpdateOrdersMetrics();
+DROP PROCEDURE BatchUpdateOrdersMetrics;
