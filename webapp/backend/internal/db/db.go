d004afe7d4d0c1704e16f27e15c2cc5dcc75530e
kotarou <kotapapa101029@yahoo.co.jp>
2025-09-27 10:35:06 +0900

    fix num of dbconnect

--- webapp/backend/internal/db/db.go
+++ webapp/backend/internal/db/db.go
@@ -37,9 +37,12 @@ func InitDBConnection() (*sqlx.DB, error) {
 	}
 	log.Println("Successfully connected to MySQL!")
 
-	dbConn.SetMaxOpenConns(25)
-	dbConn.SetMaxIdleConns(10)
-	dbConn.SetConnMaxLifetime(0)
+	// dbConn.SetMaxOpenConns(25)
+	// dbConn.SetMaxIdleConns(10)
+	// dbConn.SetConnMaxLifetime(0)
+	dbConn.SetMaxOpenConns(100)
+	dbConn.SetMaxIdleConns(50)
+	dbConn.SetConnMaxLifetime(5 * time.Minute)
 
 	return dbConn, nil
 }
