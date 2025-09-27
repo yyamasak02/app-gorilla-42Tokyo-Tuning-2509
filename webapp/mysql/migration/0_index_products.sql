2c526062981b4d0af07c0811729bdbc7b9e46e1c
yuhe-02 <domineter4649@gmail.com>
2025-09-27 17:19:04 +0900

    [FIX] migration

---
+++ webapp/mysql/migration/0_index_products.sql
@@ -0,0 +1,9 @@
+-- このファイルに記述されたSQLコマンドが、マイグレーション時に実行されます。
+USE `42Tokyo2508-db`;
+CREATE INDEX idx_products_name ON products(name);
+CREATE INDEX idx_products_description ON products(description(255));
+CREATE INDEX idx_products_name_pid ON products(name, product_id);
+CREATE INDEX idx_products_value_pid ON products(value, product_id);
+CREATE INDEX idx_products_weight_pid ON products(weight, product_id);
+
+
