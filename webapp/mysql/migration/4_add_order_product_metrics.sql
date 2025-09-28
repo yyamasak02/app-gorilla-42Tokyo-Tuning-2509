36408ffd7649ed76f093ea14cfb6f018947d729e
yuhe-02 <domineter4649@gmail.com>
2025-09-28 09:02:39 +0900

    [FIX] sql syntax error

--- webapp/mysql/migration/4_add_order_product_metrics.sql
+++ webapp/mysql/migration/4_add_order_product_metrics.sql
@@ -1,26 +1,3 @@
 USE `42tokyo2508-db`;
-
-ALTER TABLE orders
-ADD COLUMN product_weight INT,
-ADD COLUMN product_value INT;
-
-DELIMITER $$
-CREATE PROCEDURE BatchUpdateOrdersMetrics()
-BEGIN
-    DECLARE updated_rows INT DEFAULT 1;
-    WHILE updated_rows > 0 DO
-        UPDATE orders o
-        JOIN products p ON o.product_id = p.product_id
-        SET o.product_weight = p.weight, o.product_value = p.value
-        WHERE o.order_id IN (
-            SELECT order_id FROM (
-                SELECT order_id FROM orders WHERE product_weight IS NULL LIMIT 10000
-            ) as t
-        );
-        SELECT ROW_COUNT() INTO updated_rows;
-    END WHILE;
-END$$
-DELIMITER ;
-
-CALL BatchUpdateOrdersMetrics();
-DROP PROCEDURE BatchUpdateOrdersMetrics;
+ALTER TABLE orders ADD COLUMN product_weight INT, ADD COLUMN product_value INT;
+UPDATE orders o JOIN products p ON o.product_id = p.product_id SET o.product_weight = p.weight, o.product_value = p.value;
--- webapp/mysql/migration/5_add_order_product_name.sql
+++ webapp/mysql/migration/5_add_order_product_name.sql
@@ -1,28 +1,3 @@
 USE `42tokyo2508-db`;
-
-ALTER TABLE orders
-ADD COLUMN product_name VARCHAR(255);
-
-DELIMITER $$
-CREATE PROCEDURE BatchUpdateOrdersName()
-BEGIN
-    DECLARE updated_rows INT DEFAULT 1;
-    WHILE updated_rows > 0 DO
-        UPDATE orders o
-        JOIN products p ON o.product_id = p.product_id
-        SET o.product_name = p.name
-        WHERE o.order_id IN (
-            SELECT order_id FROM (
-                SELECT order_id FROM orders WHERE product_name IS NULL LIMIT 10000
-            ) as t
-        );
-        SELECT ROW_COUNT() INTO updated_rows;
-    END WHILE;
-END$$
-DELIMITER ;
-
--- プロシージャ呼び出し
-CALL BatchUpdateOrdersName();
-
--- 使い終わったら削除
-DROP PROCEDURE BatchUpdateOrdersName;
+ALTER TABLE orders ADD COLUMN product_name VARCHAR(255);
+UPDATE orders o JOIN products p ON o.product_id = p.product_id SET o.product_name = p.name;
